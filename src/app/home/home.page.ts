f997982e11696221d691ad3f80f183310c0557e8
Rafaela <rafaela.barbara.custodio@gmail.com>
2021-09-25 21:28:27 -0300

    imc classify

--- src/app/home/home.page.ts
+++ src/app/home/home.page.ts
@@ -19,9 +19,22 @@ export class HomePage {
 
   onCalculate() {
     const imc = this.weight / (this.height * this.height);
-    this.showMessage(`IMC = ${imc.toFixed(2)}`);
+    this.showMessage(`IMC = ${imc.toFixed(2)} - ${this.classifyImc(imc)}`);
   }
 
+  classifyImc(imc: number){
+    if(imc < 18.5){
+      return "MAGREZA";
+    }else if(imc >= 18.5 && imc <= 24.9){
+      return "NORMAL";
+    }else if(imc >= 25 && imc <= 29.9){
+      return "SOBREPESO"
+    }else if(imc >= 30 && imc <= 39.9){
+      return "OBESIDADE"
+    }else if(imc >= 40){
+      return "OBESIDADE GRAVE"
+    }
+  }
   async showMessage(msg: string) {
     const previousToast = await this.toastController.getTop();
     if (previousToast) {
